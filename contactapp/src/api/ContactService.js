26aa5a2a86b00481ff4259f2330c2a30c66dad8d
dreamchaser <stevenalexanderdraai@gmail.com>
2026-04-08 16:43:33 +0200

    fixed the cors bug (backend)

--- contactapp/src/api/ContactService.js
+++ contactapp/src/api/ContactService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/contacts';
+const API_URL = 'http://localhost:8080/contacts';
 
 export async function saveContact(contact) {
     return await axios.post(API_URL, contact);
